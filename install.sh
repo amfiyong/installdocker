a5dc0fcaf0d25fc23ccfa266a480efe336091bd0
amfiyong <48166955+amfiyong@users.noreply.github.com>
2020-11-12 20:09:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,11 +96,11 @@ error_detect_depends(){
 
 # install docker and docker-compose
 install_docker(){
-    echo -e "Starting Installing Docker"
+    echo -e "Starting Installing Docker "
     curl -fsSL https://get.docker.com -o get-docker.sh
     bash get-docker.sh
-    echo -e "Starting Installing Docker Compose"
-    curl -L https://github.com/docker/compose/release/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    echo -e "Starting Installing Docker Compose "
+    curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
     clear
